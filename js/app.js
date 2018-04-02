754d2af3755cb4021afac92d88ef636b333ad9d2
Luis Romero <diceluisco@gmail.com>
2018-04-01 19:28:04 -0500

    add show overlay function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const CLOSEICON = document.getElementById('overlayClose');
 const OVERLAY = document.getElementById('overlay');
+const DIRECTORY = document.getElementById('directory');
+const EMPLOYEE = DIRECTORY.getElementsByClassName('employee-box');
 
 function showOverlay() {
 	OVERLAY.style.display = '';
@@ -7,6 +9,10 @@ function showOverlay() {
 
 function hideOverlay() {
 	OVERLAY.style.display = 'none';
+}
+
+for (let iterator of EMPLOYEE) {
+	iterator.addEventListener('click', showOverlay);
 }
 
 CLOSEICON.addEventListener('click', hideOverlay);

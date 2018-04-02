5181a9da8088889b6e57c7a50485a66be729feaa
Luis Romero <diceluisco@gmail.com>
2018-04-01 21:24:03 -0500

    get api response

--- js/app.js
+++ js/app.js
@@ -3,8 +3,26 @@ const OVERLAY = document.getElementById('overlay');
 const DIRECTORY = document.getElementById('directory');
 const EMPLOYEE = DIRECTORY.getElementsByClassName('employee-box');
 
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://randomuser.me/api/?results=12');
+xhr.send();
+
+xhr.onload = function () {
+	let DONE = 4;
+	let OK = 200;
+
+	if (xhr.readyState === DONE) {
+		if (xhr.status === OK) {
+			var randomEmployee = JSON.parse(xhr.responseText);
+			console.log(randomEmployee.results);
+		} else {
+			console.log('Error: ' + xhr.status);
+		}
+	}
+}
+
 function showOverlay() {
-	OVERLAY.style.display = '';
+	OVERLAY.style.display = 'flex';
 }
 
 function hideOverlay() {
